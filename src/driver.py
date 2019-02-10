c0855433f112be67e4ade7fbb641668fde12a08b
detorresBilly <detorresbilly@gmail.com>
2019-02-10 12:49:28 -0500

    keywords and language fix

--- src/driver.py
+++ src/driver.py
@@ -9,17 +9,16 @@ g = Github("8a86a310715943bcfbc58a45168de522c2ee0d97")
 with open('to_search.json') as f:
     dev_input = json.load(f)
 
-    pprint(dev_input)
-    print("hi")
-    print(dev_input["keywords"])
+    filters = dev_input["keywords"]
+    language = dev_input["languages"]
 
 list1 = user_search("java")
 list2 = repo_search("java", search_filters="Compiler")
 
 zipper_list = list(chain.from_iterable(zip(list1, list2)))
 
-for dev in zipper_list:
-    print(dev)
+# for dev in zipper_list:
+#     print(dev)
 
 json_string = json.dumps([ob.__dict__ for ob in zipper_list])
 f = open("dev_data.json", "w")
